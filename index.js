1e2ffd7fd62c49c2128fbdb6238170c060887e31
Sayrix <43046854+Sayrixytfz@users.noreply.github.com>
2020-05-13 14:22:28 +0200

    Episode 2

--- index.js
+++ index.js
@@ -8,15 +8,44 @@ bot.on("ready", ()=> {
     console.log("Pret !")
 });
 
-bot.on("message", message => {
+bot.on("message", async message => {
     if (message.author.bot) return;
     if (message.channel.type === "dm") return;
+    if (!message.content.startsWith(prefix)) return;
 
     const cmd = message.content.split(' ')[0].slice(prefix.length);
     const args = message.content.split(' ').slice(1);
 
     if (cmd === "ping") {
         message.channel.send(`${bot.ws.ping}ms`);
+    };
+
+    if (cmd === "clear") {
+        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Vous n\'avez pas la permission requise');
+        if(!args[0]) return message.channel.send('Veuillez écrire un nombre entre 1 et 100');
+        if(args[0] === "0") return message.channel.send('Veuillez écrire un nombre entre 1 et 100');
+
+        let messageDelete = parseInt(args[0]);
+
+        if (messageDelete > 99) messageDelete = 99;
+
+        let messages = await message.channel.messages.fetch({limit: 100});
+
+        message.channel.bulkDelete(messages);
+    }
+
+    if (cmd === "help") {
+        const embed = new Discord.MessageEmbed()
+        .setAuthor(`Help`, bot.user.avatarURL())
+        .setThumbnail(bot.user.avatarURL())
+        .setDescription(`Nombre de commande : \`3\``)
+        .addField("Liste :", "`ping`, `clear`, `help`")
+        .addField("qsfdqsdf", "dfsdfsdf", true)
+        .addField("qtyhdj", "qfieuzsh", true)
+        .setTimestamp()
+        .setFooter('Bonsoir', bot.user.avatarURL())
+        .setImage(bot.user.avatarURL())
+        message.channel.send(embed)
     }
 });
 
